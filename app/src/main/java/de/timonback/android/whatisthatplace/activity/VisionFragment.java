b6f61ea0ca9990c3da4307d594f9a8a4c6e05dd5
Timon Back <T.Back@student.rug.nl>
2017-12-08 20:21:31 +0100

    Removed circleci screenshot test

---
+++ app/src/main/java/de/timonback/android/whatisthatplace/activity/VisionFragment.java
@@ -0,0 +1,53 @@
+package de.timonback.android.whatisthatplace.activity;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.timonback.android.whatisthatplace.R;
+
+
+public class VisionFragment extends Fragment {
+    private final static String LOG_NAME = VisionFragment.class.getName();
+
+
+    public VisionFragment() {
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment VisionFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static VisionFragment newInstance(String param1, String param2) {
+        VisionFragment fragment = new VisionFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vision, container, false);
+    }
+}
